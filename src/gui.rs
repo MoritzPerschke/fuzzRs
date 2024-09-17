b96a40b42c19867d7c9bc0f913fd0b571904552c
csaw4531 <moritz.perschke@student.uibk.ac.at>
2024-09-17 04:15:34 +0200

    Implemented all textboxes
    Selection of the 'matchrules' textbox doesn't work as of now, tui-textarea maps ctrl-m to newline, need to find workaround

--- src/gui.rs
+++ src/gui.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::sync::{ Arc, Mutex };
 use std::io::{ self };
+use std::thread::current;
 
 use ratatui::prelude::*;
 use reqwest::{header, StatusCode};
@@ -45,21 +46,39 @@ impl Gui {
         state.wordlist = wordlist.to_string();
         state.host = host.to_string();
 
-        let mut wordlist = TextArea::default();
-        wordlist.set_block(
-            Block::default()
-                .borders(Borders::ALL)
-                .title("Wordlist"),
-        );
+        /*
+        * 0: Target
+        * 1: Wordlist
+        * 2: Data
+        * 3: Headers
+        * 4: Match rules
+        * 5: Filter rules */
+        let mut input_fields = [
+            ("Target", TextArea::default()),
+            ("Wordlist", TextArea::default()),
+            ("Data", TextArea::default()),
+            ("Headers", TextArea::default()),
+            ("Matchrules", TextArea::default()),
+            ("Filterrules", TextArea::default()),
+        ];
+        let mut current_input: usize = 0;
+
+        for i in 0..input_fields.len(){
+            input_fields[i].1.set_block(
+                Block::default()
+                    .borders(Borders::ALL)
+                    .title(input_fields[i].0),
+            );
+        }
 
         while !self.exit {
-            terminal.draw(|frame| self.draw(frame, &mut state, &wordlist))?;
-            self.handle_events(&mut wordlist)?;
+            terminal.draw(|frame| self.draw(frame, &mut state, &input_fields))?;
+            self.handle_events(&mut input_fields, &mut current_input)?;
         }
         Ok(())
     }
     
-    fn draw(&self, f: &mut Frame, state: &mut AppState, wordlist: &TextArea) {
+    fn draw(&self, f: &mut Frame, state: &mut AppState, input_fields: &[(&str, TextArea); 6]) {
         /* Definitions of Layouts */
         let root_layout = Layout::vertical(
             vec![
@@ -82,17 +101,16 @@ impl Gui {
                 Constraint::Percentage(50), // data/header input
                 Constraint::Percentage(30), // filter/match input
             ]
-        ).split(root_layout[1]);
+        ).split(body_layout[0]);
 
-        let fm_layout = Layout::horizontal(
+        let dh_layout = Layout::horizontal(
             vec![
                 Constraint::Percentage(50),
                 Constraint::Percentage(50),
             ]
         ).split(input_layout[2]);
 
-
-        let dh_layout = Layout::horizontal(
+        let fm_layout = Layout::horizontal(
             vec![
                 Constraint::Percentage(50),
                 Constraint::Percentage(50),
@@ -112,44 +130,47 @@ impl Gui {
         f.render_stateful_widget(HeaderWidget, root_layout[0], state);
 
         /* Body */
-        // f.render_widget(targetinput{}, right_body_layout[1]);
-        // f.render_widget(wordlistinput{}, right_body_layout[1]);
-        // f.render_widget(datainput{}, right_body_layout[1]);
-        // f.render_widget(headerinput{}, right_body_layout[1]);
-        // f.render_widget(matchinput{}, right_body_layout[1]);
-        // f.render_widget(filterinput{}, right_body_layout[1]);
+        // Input fields are rendered here for convenience, as rendering them in a widget introduces
+        // some lifetime issues I don't want to deal with
+        f.render_widget(&input_fields[0].1, input_layout[0]);
+        f.render_widget(&input_fields[1].1, input_layout[1]);
+        f.render_widget(&input_fields[2].1, dh_layout[0]);
+        f.render_widget(&input_fields[3].1, dh_layout[1]);
+        f.render_widget(&input_fields[4].1, fm_layout[0]);
+        f.render_widget(&input_fields[5].1, fm_layout[1]);
         f.render_widget(EmptyWidget {title: "Results".to_string()}, right_body_layout[0]);
         f.render_widget(HelpWidget {}, right_body_layout[1]);
     }
 
-    fn handle_events(&mut self, wordlist: &mut TextArea) -> io::Result<()> {
+    fn handle_events(&mut self, input_fields: &mut [(&str, TextArea); 6], active_input: &mut usize) -> io::Result<()> {
         match crossterm::event::read()?.into() {
-            // Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-            //     self.handle_key_event(key_event)
-            // }
             Input {key: Key::Char('q'), ctrl: true, ..} => self.exit(),
             Input {key: Key::Char('r'), ctrl: true, ..} => fuzzer::fuzz(&self.state.wordlist, &self.state.host, &self.state.query_results),
+            Input {key: Key::Char('t'), ctrl: true, ..} => self.change_active_input(input_fields, 0, active_input),
+            Input {key: Key::Char('w'), ctrl: true, ..} => self.change_active_input(input_fields, 1, active_input),
+            Input {key: Key::Char('d'), ctrl: true, ..} => self.change_active_input(input_fields, 2, active_input),
+            Input {key: Key::Char('h'), ctrl: true, ..} => self.change_active_input(input_fields, 3, active_input),
+            Input {key: Key::Char('m'), ctrl: true, ..} => self.change_active_input(input_fields, 4, active_input),
+            Input {key: Key::Char('f'), ctrl: true, ..} => self.change_active_input(input_fields, 5, active_input),
             input => {
-                wordlist.input(input);
+                input_fields[*active_input].1.input(input);
             }
         };
         Ok(())
     }
 
-    // fn handle_key_event(&mut self, key_event: KeyEvent) {
-    //     match key_event.code {
-    //         KeyCode::Char('r') =>  {
-    //             self.state.title = " FuzzRs (running...) ".to_string();
-    //             fuzzer::fuzz(&self.state.wordlist, &self.state.host, &self.state.query_results);
-    //         }
-    //         KeyCode::Char('q') =>  self.exit(),
-    //         _ => {}
-    //     }
-    // }
-
     fn exit(&mut self) {
         self.exit = true;
     }
+
+    fn change_active_input(&mut self, input_fields: &mut [(&str, TextArea); 6], next_active: usize, current_active: &mut usize) {
+        input_fields[*current_active].1.set_cursor_style(Style::default());
+        input_fields[*current_active].1.set_cursor_line_style(Style::default());
+        input_fields[next_active].1.set_cursor_style(Style::default().add_modifier(Modifier::REVERSED));
+        input_fields[next_active].1.set_cursor_line_style(Style::default().add_modifier(Modifier::UNDERLINED));
+        *current_active = next_active;
+    }
+    
 }
 
 struct HeaderWidget;
@@ -220,57 +241,50 @@ impl Widget for HelpWidget {
         ).split(area);
 
         Paragraph::new(Text::from(vec![
-            Line::from(vec!["<C-t> ".bold().into(), " Set Target ".into()]), // why does it need this?
-            Line::from(vec!["<C-t> ".bold().into(), " Set target ".into()]),
-            Line::from(vec!["<C-w> ".bold().into(), " Set wordlist ".into()]),
-            Line::from(vec!["<C-d> ".bold().into(), " Edit request data ".into()]),
-            Line::from(vec!["<C-h> ".bold().into(), " Edit request headers ".into()]),
-            Line::from(vec!["<C-m> ".bold().into(), " Edit matching rules ".into()]),
-            Line::from(vec!["<C-f> ".bold().into(), " Edit filter rules ".into()]),
-        ]))
-        .centered()
-        .render(layout[1], buf);
-
-        Paragraph::new(Text::from(vec![
-            Line::from(vec!["<C-r> ".bold().into(), " Run fuzzer ".into()]),
-            Line::from(vec!["<C-q> ".bold().into(), " Quit fuzzer ".into()]),
-            Line::from(vec!["<C-s> ".bold().into(), " Edit settings ".into()]),
+            Line::from(vec!["<C-t> ".bold().into(), " Set target ".into()]).alignment(Alignment::Left),
+            Line::from(vec!["<C-w> ".bold().into(), " Set wordlist ".into()]).alignment(Alignment::Left),
+            Line::from(vec!["<C-d> ".bold().into(), " Edit request data ".into()]).alignment(Alignment::Left),
+            Line::from(vec!["<C-h> ".bold().into(), " Edit request headers ".into()]).alignment(Alignment::Left),
+            Line::from(vec!["<C-m> ".bold().into(), " Edit matching rules ".into()]).alignment(Alignment::Left),
+            Line::from(vec!["<C-f> ".bold().into(), " Edit filter rules ".into()]).alignment(Alignment::Left),
         ]))
         .centered()
         .block(
                 Block::bordered()
                 .title(Title::from(" Help ".bold()).alignment(Alignment::Left))
-                .border_set(border::ROUNDED)
+                .border_set(border::PLAIN)
             )
         .render(layout[0], buf);
 
-        Paragraph::new("")
+        Paragraph::new(Text::from(vec![
+            Line::from(vec!["<C-r> ".bold().into(), " Run fuzzer ".into()]).alignment(Alignment::Left),
+            Line::from(vec!["<C-q> ".bold().into(), " Quit fuzzer ".into()]).alignment(Alignment::Left),
+            Line::from(vec!["<C-s> ".bold().into(), " Edit settings ".into()]).alignment(Alignment::Left),
+        ]))
+        .centered()
         .block(
                 Block::bordered()
-                .title(Title::from(" Help ".bold()).alignment(Alignment::Center))
-                .border_set(border::ROUNDED)
+                .border_set(border::PLAIN)
             )
-        .render(area, buf)
+        .render(layout[1], buf);
 
     }
 }
 
-struct InputWidget{
-    state: AppState,
-}
-impl StatefulWidget for InputWidget {
-    type State = AppState;
-    fn render(self, area:Rect, buf: &mut Buffer, state: &mut AppState){
-
-
-        EmptyWidget {title: state.host.to_string()}.render(settings_layout[0], buf);
-        state.wordlist.render(settings_layout[1], buf);
-        EmptyWidget {title: "Data".to_string()}.render(dh_layout[0], buf);
-        EmptyWidget {title: "Headers".to_string()}.render(dh_layout[1], buf);
-        EmptyWidget {title: "Match".to_string()}.render(fm_layout[0], buf);
-        EmptyWidget {title: "Filter".to_string()}.render(fm_layout[1], buf);
-    }
-}
+// struct InputWidget{
+//     state: AppState,
+// }
+// impl StatefulWidget for InputWidget {
+//     type State = AppState;
+//     fn render(self, area:Rect, buf: &mut Buffer, state: &mut AppState){
+//         EmptyWidget {title: state.host.to_string()}.render(settings_layout[0], buf);
+//         state.wordlist.render(settings_layout[1], buf);
+//         EmptyWidget {title: "Data".to_string()}.render(dh_layout[0], buf);
+//         EmptyWidget {title: "Headers".to_string()}.render(dh_layout[1], buf);
+//         EmptyWidget {title: "Match".to_string()}.render(fm_layout[0], buf);
+//         EmptyWidget {title: "Filter".to_string()}.render(fm_layout[1], buf);
+//     }
+// }
 
 struct EmptyWidget {
     title: String,
@@ -279,7 +293,7 @@ impl Widget for EmptyWidget {
     fn render(self, area: Rect, buf: &mut Buffer){
         Paragraph::new(self.title)
             .centered()
-            .block(Block::bordered().border_set(border::ROUNDED))
+            .block(Block::bordered().border_set(border::PLAIN))
             .render(area, buf)
     }
 }
